17570007e4ae631edbeb25ed1a0eb1ba2ce09047
Matvey <matvey.ivanov.person@yandex.ru>
2024-01-02 18:16:22 +0700

    v2

--- main.py
+++ main.py
@@ -4,63 +4,75 @@ import random
 def draw_table():
     return (f"     A   B   C  \n"
             f"   —————————————\n"
-            f"1  | {cells[1]} | {cells[2]} | {cells[3]} |\n"
+            f"1  | {board[0][0]} | {board[0][1]} | {board[0][2]} |\n"
             f"   —————————————\n"
-            f"2  | {cells[4]} | {cells[5]} | {cells[6]} |\n"
+            f"2  | {board[1][0]} | {board[1][1]} | {board[1][2]} |\n"
             f"   —————————————\n"
-            f"3  | {cells[7]} | {cells[8]} | {cells[9]} |\n"
+            f"3  | {board[2][0]} | {board[2][1]} | {board[2][2]} |\n"
             f"   —————————————")
 
 
-def step():
-    cell_valid = False
-    while not cell_valid:
-        cell = input("Клетка: ")
-        if cell in cell_names and cells[cell_names.index(cell) + 1] not in (machine_symbol, player_symbol):
-            cell_valid = True
-        else:
-            print("[!] Вы ввели неверное значение.")
-    cells[cell_names.index(cell) + 1] = player_symbol
-
-
-def random_step():
-    random_index_valid = False
-    while not random_index_valid:
-        random_index = random.randint(1, 9)
-        if cells[random_index] not in (machine_symbol, player_symbol):
-            cells[random_index] = machine_symbol
-            random_index_valid = True
-
-
 def win():
-    if ((cells[1] == cells[2] == cells[3] == player_symbol)
-            or (cells[4] == cells[5] == cells[6] == player_symbol)
-            or (cells[7] == cells[8] == cells[9] == player_symbol)
-            or (cells[1] == cells[4] == cells[7] == player_symbol)
-            or (cells[2] == cells[5] == cells[8] == player_symbol)
-            or (cells[3] == cells[6] == cells[9] == player_symbol)
-            or (cells[1] == cells[5] == cells[9] == player_symbol)
-            or (cells[3] == cells[5] == cells[7] == player_symbol)):
-        print("Вы выиграли!")
-        return True
-    if ((cells[1] == cells[2] == cells[3] == machine_symbol)
-            or (cells[4] == cells[5] == cells[6] == machine_symbol)
-            or (cells[7] == cells[8] == cells[9] == machine_symbol)
-            or (cells[1] == cells[4] == cells[7] == machine_symbol)
-            or (cells[2] == cells[5] == cells[8] == machine_symbol)
-            or (cells[3] == cells[6] == cells[9] == machine_symbol)
-            or (cells[1] == cells[5] == cells[9] == machine_symbol)
-            or (cells[3] == cells[5] == cells[7] == machine_symbol)):
-        print("Вы проиграли.")
-        return True
-    if (cells[1] != " " and cells[2] != " " and cells[3] != " " and cells[4] != " " and cells[5] != " "
-            and cells[6] != " " and cells[7] != " " and cells[8] != " " and cells[9] != " "):
+    for i in wins:
+        if (board[i[0][0]][i[0][1]] == board[i[1][0]][i[1][1]]
+                == board[i[2][0]][i[2][1]] == player_symbol):
+            print("Вы выиграли!")
+            return True
+        if (board[i[0][0]][i[0][1]] == board[i[1][0]][i[1][1]]
+                == board[i[2][0]][i[2][1]] == machine_symbol):
+            print("Вы проиграли!")
+            return True
+
+
+def draw():
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] != " ":
+                count += 1
+    if count == 9:
         print("Ничья.")
         return True
 
 
-cell_names = ("A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3")
-cells = {1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: " "}
+def random_step():
+    valid = False
+    while not valid:
+        rnd_step = [random.randint(0, 2), random.randint(0, 2)]
+        if board[rnd_step[0]][rnd_step[1]] not in (
+                player_symbol, machine_symbol):
+            board[rnd_step[0]][rnd_step[1]] = machine_symbol
+            valid = True
+
+
+def step():
+    valid = False
+    while not valid:
+        cell = input("Клетка: ")
+        if ((len(list(cell)) == 2) and (list(cell)[0] in ("A", "B", "C")) and
+                (list(cell)[1] in ("1", "2", "3")) and
+                (board[cells[list(cell)[1]]][cells[list(cell)[0]]] == " ")):
+            valid = True
+        else:
+            print("[!] Вы ввели неверное значение.")
+    board[cells[list(cell)[1]]][cells[list(cell)[0]]] = player_symbol
+
+
+board = [[" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
+
+cells = {"A": 0, "B": 1, "C": 2,
+         "1": 0, "2": 1, "3": 2}
+
+wins = [[[0, 0], [0, 1], [0, 2]],
+        [[1, 0], [1, 1], [1, 2]],
+        [[2, 0], [2, 1], [2, 2]],
+        [[0, 0], [1, 0], [2, 0]],
+        [[0, 1], [1, 1], [2, 1]],
+        [[0, 2], [1, 2], [2, 2]],
+        [[0, 0], [1, 1], [2, 2]],
+        [[0, 2], [1, 1], [2, 0]]]
 
 queue = random.randint(1, 2)
 if queue == 1:
@@ -80,5 +92,5 @@ while start:
     step()
     random_step()
     print(draw_table())
-    if win():
+    if win() or draw():
         start = False
